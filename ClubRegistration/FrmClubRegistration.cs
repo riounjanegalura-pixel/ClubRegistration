a2edafe25389f773c85ba40df24586e9c048bbc9
Rio Unjane Galura <riounjanegalura@gmail.com>
2025-11-29 17:08:18 +0800

    declare variables

--- ClubRegistration/FrmClubRegistration.cs
+++ ClubRegistration/FrmClubRegistration.cs
@@ -12,9 +12,24 @@ namespace ClubRegistration
 {
     public partial class FrmClubRegistration : Form
     {
+        private ClubRegistrationQuery clubRegistrationQuery;
+
+        private int ID;
+        private int Age;
+        private int count;
+
+        private string FirstName;
+        private string MiddleName;
+        private string LastName;
+        private string Gender;
+        private string Program;
+
+        private long StudentId;
+
         public FrmClubRegistration()
         {
             InitializeComponent();
+            clubRegistrationQuery = new ClubRegistrationQuery();
         }
     }
 }
