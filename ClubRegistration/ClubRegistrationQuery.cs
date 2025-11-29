f62a2e02fb885985f82d16ff0d564b8472d96b59
Rio Unjane Galura <riounjanegalura@gmail.com>
2025-11-29 16:49:11 +0800

    club registration query class

---
+++ ClubRegistration/ClubRegistrationQuery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ClubRegistration
+{
+    public class ClubRegistrationQuery
+    {
+        private SqlConnection sqlConnect;
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlAdapter;
+        private SqlDataReader sqlReader;
+        private string connectionString;
+
+        public DataTable dataTable;
+        public BindingSource bindingSource;
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Program { get; set; }
+        public int Age { get; set; }
+    }
+}
