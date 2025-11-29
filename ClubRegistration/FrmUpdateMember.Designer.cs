8eac10b7f52f6faf1c1aaa427ae58694cf331560
Rio Unjane Galura <riounjanegalura@gmail.com>
2025-11-29 16:35:43 +0800

    UI

--- ClubRegistration/FrmUpdateMember.Designer.cs
+++ ClubRegistration/FrmUpdateMember.Designer.cs
@@ -43,6 +43,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -181,11 +182,21 @@
             this.button1.Text = "Confirm";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 58);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Confirm";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FrmUpdateMember
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(672, 367);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
@@ -225,5 +236,6 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
